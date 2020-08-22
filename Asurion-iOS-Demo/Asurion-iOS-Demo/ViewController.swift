a8ad0eb8d5a9d9d6e68657db06cd5c1d8dbac411
TYLER <megatyx@gmail.com>
2020-08-23 01:16:02 +0900

    first commit iOS client

---
+++ Asurion-iOS-Demo/Asurion-iOS-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Asurion-iOS-Demo
+//
+//  Created by Tyler Wells on 8/23/20.
+//  Copyright © 2020 Tyler Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
